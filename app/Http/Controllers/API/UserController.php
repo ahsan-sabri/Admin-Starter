fde42a7c053cac368e803ae41e4e57aae13ca8c6
Ahsan Sabri <ahsan.sabri_me08@yahoo.com>
2020-09-27 22:03:44 +0600

    model location move to Models folder

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\User;
+use App\Models\Auth\User;
 use Illuminate\Support\Facades\Hash;
 
 
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -46,7 +46,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{ url('/') }}" class="brand-link">
-                <img src="./img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                <img src="{{ asset('img/logo.png') }}" alt="AdminLTE Logo" class="brand-image elevation-3"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
             </a>
